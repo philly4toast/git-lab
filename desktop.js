f260734a74b62f8f5f7575b60de5df7e3f9e0c83
James Youn <jamesyoun710@gmail.com>
2020-01-28 16:51:18 -0800

    Fix bad spacing

--- desktop.js
+++ desktop.js
@@ -1,15 +1,16 @@
 function fillGrid(n) {
   var result = [];
   var innerArray = [];
-var count = 1
-for (i = 0; i < n; i++){
-for (var j = 0; j < n; j++){
-innerArray.push(count)
-count++;
-}
-result.push(innerArray)
-}
-return result;
+  var count = 1
+
+  for (i = 0; i < n; i++){
+    for (var j = 0; j < n; j++){
+      innerArray.push(count)
+      count++;
+    }
+    result.push(innerArray)
+  }
+  return result;
 }
 
 fillGrid(2)
